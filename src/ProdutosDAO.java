96079b43ca83bf2064dddd61847f976d435663b2
Ricardo <peaceandhope702@gmai.com>
2026-05-22 03:09:43 -0300

    Tela de vendas e listagem de produtos vendidos implementadas

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -76,11 +76,73 @@ public void venderProduto(int id){
 
 public ArrayList<ProdutosDTO> listarProdutos(){
 
+    String sql = "SELECT * FROM produtos";
+
+    conn = new conectaDAO().connectDB();
+
+    try {
+
+        prep = conn.prepareStatement(sql);
+
+        resultset = prep.executeQuery();
+
+        while(resultset.next()){
+
+            ProdutosDTO produto = new ProdutosDTO();
+
+            produto.setId(resultset.getInt("id"));
+            produto.setNome(resultset.getString("nome"));
+            produto.setValor(resultset.getInt("valor"));
+            produto.setStatus(resultset.getString("status"));
+
+            listagem.add(produto);
+
+        }
+
+    } catch (Exception e) {
+
+        JOptionPane.showMessageDialog(null, "Erro ao listar produtos: " + e.getMessage());
+
+    }
+
     return listagem;
 }
     
-    
-    
+public ArrayList<ProdutosDTO> listarProdutosVendidos(){
+
+    String sql = "SELECT * FROM produtos WHERE status = 'Vendido'";
+
+    conn = new conectaDAO().connectDB();
+
+    try {
+
+        prep = conn.prepareStatement(sql);
+
+        resultset = prep.executeQuery();
+        listagem.clear();
+
+        while(resultset.next()){
+
+            ProdutosDTO produto = new ProdutosDTO();
+
+            produto.setId(resultset.getInt("id"));
+            produto.setNome(resultset.getString("nome"));
+            produto.setValor(resultset.getInt("valor"));
+            produto.setStatus(resultset.getString("status"));
+
+            listagem.add(produto);
+
+        }
+
+    } catch (Exception e) {
+
+        JOptionPane.showMessageDialog(null, "Erro ao listar vendidos: " + e.getMessage());
+
+    }
+
+    return listagem;
+}
         
 }
+
 
